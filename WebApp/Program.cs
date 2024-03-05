0821e457d9b1d707544337a0d2de1505f6f2c489
George Hira <135507784+george-hira@users.noreply.github.com>
2024-03-05 21:56:48 +0200

    Add project files.

---
+++ WebApp/Program.cs
@@ -0,0 +1,22 @@
+using System.Net.Mime;
+
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+// first part reffers the name of the controller 
+// second part reffers to the name of action method 
+// third part is optional and it reffers to the paramether of the action method 
+app.Run();
+
+
+

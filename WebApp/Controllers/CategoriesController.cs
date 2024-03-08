158417cf269748e923a8ee81b6f61d9286af34b5
George Hira <135507784+george-hira@users.noreply.github.com>
2024-03-08 17:42:29 +0200

    using Model Binding

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -11,13 +11,21 @@ namespace WebApp.Controllers
             return View(categories);
         }
 
-
+        
         public IActionResult Edit(int? id)
         {
-            var category = new Category { CategoryId =  id.HasValue?id.Value:0 };
+            var category= CategoriesRepository.GetCategoryByID(id.HasValue?id.Value:0);
 
             return View(category);
        
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category) 
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+
+            return RedirectToAction(nameof(Index));
+        
         }
     }
 }
